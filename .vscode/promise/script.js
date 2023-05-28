f7f685d079ff9a08c603558ad1f22f158aad0862
fuzail khan <fuzailkhanbt@gmail.com>
2023-05-28 13:37:57 -0700

    promise adding

---
+++ .vscode/promise/script.js
@@ -0,0 +1,43 @@
+// function fetchData() {
+//     return new Promise((resolve, reject) => {
+//       // Simulating an asynchronous operation (e.g., API call or data fetching)
+//       setTimeout(() => {
+//         const data = { name: "John", age: 25 };
+//         if (data) {
+//           resolve(data); // Resolving the promise with the data
+//         } else {
+//           reject("Error: Data not found"); // Rejecting the promise with an error message
+//         }
+//       }, 2000); // Simulating a delay of 2 seconds
+//     });
+//   }
+  
+//   // Using the created promise
+//   fetchData()
+//     .then((data) => {
+//       console.log("Fulfilled:", data);
+//     })
+//     .catch((error) => {
+//       console.log("Rejected:", error);
+//     });
+
+const makePromise = new Promise ((resolve , reject ) =>{
+    setTimeout(() =>{
+        const arrOne = ["reactjs " , "redux" , "typescript"]
+        if(arrOne.length > 3 ){
+            resolve("completed")
+
+        }else{
+            reject ("incomplete")
+
+        }
+    } , 2000)
+
+    
+})
+makePromise.then((result) =>{
+    console.log(result)
+
+}).catch((result) =>{
+    console.log(result)
+})
